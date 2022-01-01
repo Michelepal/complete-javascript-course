d4534ba8b1fd97f4fea1c295ca2a46557e764616
Michelepal <mikaelquasimodus@gmail.com>
2022-01-01 09:09:52 +0100

    lezione 3 listener eventi click

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,4 +1,26 @@
 'use strict';
 
-//lezione 1 
-document.querySelector('.message').textContent;
+/* //lezione 1 - selezione elementi DOM
+
+
+document.querySelector('.message').textContent = 'Correct Number!';
+
+//lezione 2 - selezione elementi DOM 2
+
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.number').textContent = 13;
+
+document.querySelector('.guess').value =  23;
+
+*/
+
+//lezione 3 - Listener eventi click
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No Number!';
+  }
+});
