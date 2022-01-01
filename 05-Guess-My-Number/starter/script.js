c6641a9ef3655a32680b9805abbbc4aae7abac47
Michelepal <mikaelquasimodus@gmail.com>
2022-01-01 09:46:33 +0100

    fix

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -34,9 +34,14 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = 0;
     }
   } else if (guess < secretNumber) {
-    document.querySelector('.message').textContent = 'Too Low!';
-    score--;
-    document.querySelector('.score').textContent = score;
+    if (score > 1) {
+        document.querySelector('.message').textContent = 'Too Low!';
+        score--;
+        document.querySelector('.score').textContent = score;
+      } else {
+        document.querySelector('.message').textContent = 'You Lost!';
+        document.querySelector('.score').textContent = 0;
+      }
   }
 });
 
