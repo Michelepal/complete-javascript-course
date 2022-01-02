a5e958fc95c5cb3e2cf5426155fd0b300cc294c8
Michelepal <mikaelquasimodus@gmail.com>
2022-01-02 09:24:35 +0100

    lezione 4 modifica del css

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -24,6 +24,10 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = 'No Number!';
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct Number!';
+
+    //lezione 4 modifica del csss.
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too high!';
@@ -35,16 +39,23 @@ document.querySelector('.check').addEventListener('click', function () {
     }
   } else if (guess < secretNumber) {
     if (score > 1) {
-        document.querySelector('.message').textContent = 'Too Low!';
-        score--;
-        document.querySelector('.score').textContent = score;
-      } else {
-        document.querySelector('.message').textContent = 'You Lost!';
-        document.querySelector('.score').textContent = 0;
-      }
+      document.querySelector('.message').textContent = 'Too Low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You Lost!';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 document.querySelector('.number').textContent = secretNumber;
 let score = 20;
+
+document.querySelector('.again').addEventListener('click', function () {
+  document.querySelector('.score').textContent = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.number').textContent = 0;
+});
