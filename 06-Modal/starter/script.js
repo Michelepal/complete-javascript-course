1da7658a57a6239cfdeb03caa5dacfaa30dbd6b5
Michelepal <mikaelquasimodus@gmail.com>
2022-01-04 10:02:26 +0100

    lezione 2

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,25 @@
 'use strict';
+//lezione 1
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+// lezione 2 
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', openModal);
+}
+
+const openModal = function() {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+}
+const closeModal = function() {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
